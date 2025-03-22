ce450806af3d8a1a87a973dbfcde489b520bab7b
Wilian Corrêa <correa@wilian.dev.br>
2025-03-22 12:30:19 -0300

    implementado as configurações customizadas para o nginx

--- services/nginx/latest/entrypoint.sh
+++ services/nginx/latest/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-# Substitui a variável de ambiente PUBLIC_PATH no arquivo de configuração do Nginx
-envsubst '$PUBLIC_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
-# Inicia o Nginx
+# Verificar se a variável CUSTOM_CONFIG_ENABLED está habilitada e se o arquivo custom.conf existe
+if [ "$CUSTOM_CONFIG_ENABLED" = "true" ] && [ -f "/tmp/custom.conf" ]; then
+    # Se a configuração personalizada estiver habilitada e o arquivo custom.conf existir, usá-lo
+    envsubst '$PUBLIC_PATH' < "/tmp/custom.conf" > "/etc/nginx/conf.d/default.conf"
+else
+    # Caso contrário, utilizar a configuração padrão (nginx.conf)
+    envsubst '$PUBLIC_PATH' < "/tmp/nginx.conf" > "/etc/nginx/conf.d/default.conf"
+fi
+
+# Iniciar o Nginx em primeiro plano, mantendo o container em execução
 exec nginx -g "daemon off;"
