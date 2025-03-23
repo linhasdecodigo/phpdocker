8886631db6f9f742114899feb982dff33d0dfa5b
Wilian Corrêa <correa@wilian.dev.br>
2025-03-23 18:33:51 -0300

    corrigidos erros e organizado algumas coisas

---
+++ services/nginx/1.27/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Verificar se a variável NGINX_CUSTOM_CONF_ENABLED está habilitada e se o arquivo custom.conf existe
+if [ "$NGINX_CUSTOM_CONF_ENABLED" = "true" ] && [ -f "/tmp/nginx-custom.conf" ]; then
+    rm -f "/etc/nginx/conf.d/default.conf"
+
+    # Se a configuração personalizada estiver habilitada e o arquivo custom.conf existir, usá-lo
+    envsubst '$APP_PUBLIC' < "/tmp/nginx-custom.conf" > "/etc/nginx/conf.d/default.conf"
+else
+    if [ -f "/tmp/nginx.conf" ]; then
+        rm -f "/etc/nginx/conf.d/default.conf"
+        
+        # Caso contrário, utilizar a configuração padrão (nginx.conf)
+        envsubst '$APP_PUBLIC' < "/tmp/nginx.conf" > "/etc/nginx/conf.d/default.conf"
+    fi
+fi
+
+# Iniciar o Nginx em primeiro plano, mantendo o container em execução
+exec nginx -g "daemon off;"
